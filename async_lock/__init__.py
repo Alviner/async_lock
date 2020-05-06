67e5192a175bfd54e79866bca295b86d1056469a
Ivan Sitkin <alviner@yandex-team.ru>
2020-05-06 20:07:38 +0300

    Bump version: 0.0.0 → 0.0.1

--- async_lock/__init__.py
+++ async_lock/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for async-lock."""
 
-__author__ = """Ivan Sitkin"""
-__email__ = "alvinera@yandex.ru"
+__author__ = 'Ivan Sitkin'
+__email__ = 'alvinera@yandex.ru'
 __version__ = '0.0.0'
--- setup.py
+++ setup.py
@@ -1,53 +1,52 @@
 #!/usr/bin/env python
 
-"""The setup script."""
 
 from setuptools import setup, find_packages
 from pkg_resources import parse_requirements
 
-with open("README.rst") as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 
 def load_requirements(fname) -> list:
     requirements = []
-    with open(fname, "r") as fp:
+    with open(fname, 'r') as fp:
         for req in parse_requirements(fp.read()):
             name = req.name
             if req.extras:
-                name += "[" + ",".join(req.extras) + "]"
+                name += '[' + ','.join(req.extras) + ']'
             requirements.append(name + str(req.specifier))
         return requirements
 
 
 setup(
-    author="Ivan Sitkin",
-    author_email="alvinera@yandex.ru",
-    python_requires=">=3.5",
+    author='Ivan Sitkin',
+    author_email='alvinera@yandex.ru',
+    python_requires='>=3.5',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    description="Wrapper to provide distributed locks with some brokers"
-    "(eg aiopg)",
-    install_requires=load_requirements("requirements.txt"),
-    license="MIT license",
-    long_description=readme + "\n\n" + history,
+    description='Wrapper to provide distributed locks with some brokers'
+    '(eg aiopg)',
+    install_requires=load_requirements('requirements.txt'),
+    license='MIT license',
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords="async_lock",
-    name="async_lock",
-    packages=find_packages(include=["async_lock", "async_lock.*"]),
-    extras_require={"develop": load_requirements("requirements.dev.txt"),},
-    test_suite="tests",
-    url="https://github.com/alviner/async_lock",
+    keywords='async_lock',
+    name='async_lock',
+    packages=find_packages(include=['async_lock', 'async_lock.*']),
+    extras_require={'develop': load_requirements('requirements.dev.txt'),},
+    test_suite='tests',
+    url='https://github.com/alviner/async_lock',
     version='0.0.0',
     zip_safe=False,
 )
